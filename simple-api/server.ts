cd1568d8b9a930f2c19b444b75c81daab0594a39
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2022-09-19 19:20:26 -0300

    init/api

---
+++ simple-api/server.ts
@@ -0,0 +1,13 @@
+import http from 'http';
+const processID = process.pid
+
+const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+			for(let index = 0; index < 1e7; index++) {
+				res.end("pid: " + processID)
+			}	
+})
+
+
+server.listen(3000).once('listening', () => {
+	console.log("Process running in: " + processID)
+})
